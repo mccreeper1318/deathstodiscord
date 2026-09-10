2313ed172257ed47cb0acb3f75833033468dfebc
Kevin <261039506+mccreeper1318@users.noreply.github.com>
2026-09-10 09:15:01 -0400

    Test trailing slash webhook message URLs

--- src/test/java/com/pinnacle/deathstodiscord/DiscordWebhookClientTest.java
+++ src/test/java/com/pinnacle/deathstodiscord/DiscordWebhookClientTest.java
@@ -20,6 +20,16 @@ class DiscordWebhookClientTest {
     }
 
     @Test
+    void messageRequestsNormalizeATrailingWebhookSlash() {
+        HttpRequest request = DiscordWebhookClient.buildDeleteRequest(
+                "https://discord.com/api/webhooks/123/token/?thread_id=456", "789");
+
+        assertEquals(
+                "https://discord.com/api/webhooks/123/token/messages/789?thread_id=456",
+                request.uri().toString());
+    }
+
+    @Test
     void creationAddsWaitWhenTheWebhookHasNoQuery() {
         assertEquals(
                 "https://discord.com/api/webhooks/123/token?wait=true",
